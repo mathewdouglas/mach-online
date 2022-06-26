6f858a1c5d8be8a8a57646f54205366160f75c9e
Mathew Douglas <mathewdouglas92@gmail.com>
2022-06-26 17:39:01 +1200

    Update database login details

--- db_connection.php
+++ db_connection.php
@@ -1,5 +1,5 @@
 <?php
-$connection = mysqli_connect("127.0.0.1:3306", "u810484942_mathewdouglas", "Jpue/CKGj*1", "u810484942_db");
+$connection = mysqli_connect("127.0.0.1:3306", "u163583148_mathewdouglas", "Jpue/CKGj*1", "u163583148_machonline");
 if (!$connection) {
     echo "Cannot connect to MySQL. ", mysqli_connect_error($connection);
     header("Location: index.php?Connection=False");
