522a9a762f75e5125d7bc2907ed564e416725fff
Mathew Douglas <mathewdouglas92@gmail.com>
2022-06-26 17:36:29 +1200

    Update

--- homepage.php
+++ homepage.php
@@ -14,8 +14,8 @@ include "userinfo.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#3162a5">
     <meta name="description" content="Germany's best freelance website">
-    <title>ODFU</title>
-    <link rel="stylesheet" href="css/style.css?v=0.36">
+    <title>Mach Online</title>
+    <link rel="stylesheet" href="css/style.css?v=0.37">
     <link rel="stylesheet" href="css/common.css?v=0.1">
     <link rel="stylesheet" href="css/mobile.css?v=0.25.3">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
